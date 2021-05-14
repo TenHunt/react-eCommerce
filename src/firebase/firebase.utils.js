2e92443b927a0f18dd3ae23dc2eb5ec0a76c3c05
Shubham <samantaray.shubham423@gmail.com>
2021-05-15 00:21:38 +0530

    added fire base and sign up page include ablity to store authenticatred user to store in data base.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import 'firebase/auth';
 const config = {
   apiKey: "AIzaSyDlE52CZMA0jk0SRR-JDNbISrxwiTMwrF8",
   authDomain: "crwn-db-b3a1b.firebaseapp.com",
+  databaseURL: '',
   projectId: "crwn-db-b3a1b",
   storageBucket: "crwn-db-b3a1b.appspot.com",
   messagingSenderId: "112523540792",
@@ -12,6 +13,32 @@ const config = {
   measurementId: "G-2EZ4C6CENC"
 };
 
+export const createUserProfileDocument = async ( userAuth, additionalData ) => {
+  if (!userAuth) return;
+  
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get()
+
+  if(!snapShot.exists) {
+    const {displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set( {
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  return userRef;
+};
 
 firebase.initializeApp(config);
 
